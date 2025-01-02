fe21b99943f02d4d2ab70563fcc94298d49c4f63
Dan4ikk <da8493137@gmail.com>
2025-01-02 16:21:43 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,6 @@
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "С писюном блять поиграй", -- Required
+	Text = "Ах ты ебанный читер", -- Required
+	Icon = "rbxassetid://" -- Optional
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DarkDoorsKing/Doors/main/Main"))()
